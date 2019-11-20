adfad3e8dd875e58ab7bca97619a480ca2d775f7
dung-phan <madewithcrayons@gmail.com>
2019-11-20 13:55:22 +0100

    load map of amsterdam on the screen

--- components/Map.js
+++ components/Map.js
@@ -1,7 +1,26 @@
 import React, { Component } from "react";
-import { Text, StyleSheet, View } from "react-native";
+import {
+  Text,
+  StyleSheet,
+  View,
+  Dimensions,
+  ActivityIndicator
+} from "react-native";
 import { Button } from "react-native-elements";
+import MapView from "react-native-maps";
 export default class Map extends Component {
+  state = {
+    mapLoaded: false,
+    region: {
+      longitude: 4.9,
+      latitude: 52.38,
+      longitudeDelta: 0.06,
+      latitudeDelta: 0.1
+    }
+  };
+  componentDidMount() {
+    this.setState({ mapLoaded: true });
+  }
   static navigationOptions = ({ navigation }) => ({
     title: "Find nearest location",
     headerRight: (
@@ -16,12 +35,31 @@ export default class Map extends Component {
   });
 
   render() {
+    if (!this.state.mapLoaded) {
+      return (
+        <View style={{ flex: 1, justifyContent: "center" }}>
+          <ActivityIndicator size="large" />
+        </View>
+      );
+    }
     return (
-      <View>
-        <Text> textInComponent </Text>
+      <View style={styles.container}>
+        <MapView region={this.state.region} style={styles.mapStyle} />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  mapStyle: {
+    flex: 1,
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height
+  }
+});
